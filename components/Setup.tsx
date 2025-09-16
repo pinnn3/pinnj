b0facabcd7b51714dedb37750d0389269ef5ef0e
pinnn3 <pinnn3@anjay.id>
2025-09-16 08:11:35 +0700

    Update Setup.tsx

--- components/Setup.tsx
+++ components/Setup.tsx
@@ -57,14 +57,14 @@ const Setup: React.FC<SetupProps> = ({ geminiApiKey, veoApiKey, onSave }) => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <ApiKeyInputSection
           title="Gemini API Key"
-          description="Used for generating JSON prompts in the 'Generator' tab."
+          description="Pakai dari 'https://aistudio.google.com/apikey' menggunakan akun gmail"
           value={localGeminiKey}
           onChange={(e) => setLocalGeminiKey(e.target.value)}
           isConfigured={!!geminiApiKey}
         />
         <ApiKeyInputSection
           title="VEO API Key"
-          description="Used for creating videos from prompts in the 'Dashboard' tab."
+          description="Pakai dari Gemini API, dari labs 'console google'"
           value={localVeoKey}
           onChange={(e) => setLocalVeoKey(e.target.value)}
           isConfigured={!!veoApiKey}
